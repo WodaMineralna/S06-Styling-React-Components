8c1c5ee68cb0be642a1330bfa0dbdabe34f0cc30
WodaMineralna <raknieborak65@gmail.com>
2025-08-15 13:46:31 +0200

    feat: add complete project built during course section 06
    - Project completed locally during course
    - Now added to repository for version control and publishing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+console.clear();
+import AuthInputs from "./components/AuthInputs.jsx";
+import Header from "./components/Header.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
